a2a34efe4e5a0221e85622d621ceddd3d6b3f6af
Yoseph <yosephtaddese111@gmail.com>
2021-11-22 18:45:59 +0300

    deployment fix for handleResponse

--- functions/utils/handleResponse.js
+++ functions/utils/handleResponse.js
@@ -2,11 +2,11 @@ const handleResponse = (req,res,body,status=200)=>{
      
      res.set('Access-Control-Allow-Origin', '*');
 
-     if (req.method === 'OPTIONS') {
        // Send response to OPTIONS requests
-       res.set('Access-Control-Allow-Methods', 'GET');
-       res.set('Access-Control-Allow-Headers', 'Content-Type');
+       res.set('Access-Control-Allow-Methods', '*');
+       res.set('Access-Control-Allow-Headers', '*');
        res.set('Access-Control-Max-Age', '3600');
+     if (req.method === 'OPTIONS') {
        res.status(204).send('');
      } else {
           res.status(status).send(body);
